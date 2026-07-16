4d5c1cecb500f3193d1e9362d086874fc08fcbed
gernotzenz <gernot.zenz@timetac.com>
2026-07-16 19:28:18 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#F4E8CC",
+  themeColor: "#f1dec6",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
